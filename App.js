f074cdcdf97b4855f276a1d56c4bd38086ddadee
omar <omohamed@pshsa.ca>
2021-01-09 14:50:47 -0500

    animated containers

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, SafeAreaView, Platform, UIManager } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Panel from './Panel';
+
+export default class App extends Component {
+  constructor(props){  
+    super(props);
+    
+    // Disabled by default on Android
+    if (Platform.OS === 'android') {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={[styles.main]}>
+        <Text style={styles.toolbar}>Animated containers</Text>
+
+        <View style={styles.content}>
+          <Panel title={'Container 1'} style={styles.panel}>
+            <Text style={styles.panelText}>Temporibus autem quibusdam et aut officiis saepe</Text>
+          </Panel>
+
+          <Panel title={'Container 2'} style={styles.panel}>
+            <Text style={styles.panelText}>
+              Et harum quidem rerum facilis est et expedita ptio cumque.Et harum quidem rerum facilis est et 
+                expedita ptio cumque. Et harum quidem rerum facilis est et expedita ptio cumque. Et harum quidem 
+                rerum facilis est et expedita ptio cumque
+            </Text>
+          </Panel>
+
+          <Panel expanded title={'Container 3'} style={styles.panel}>
+            <Text style={styles.panelText}>Nullam lobortis eu lorem ut vulputate.</Text>
+            <Text style={styles.panelText}>Donec id elementum orci. Donec fringilla lobortis</Text>
+          </Panel>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  toolbar: {
+    backgroundColor: '#3498db',
+    color: '#fff',
+    fontSize: 22,
+    padding: 20,
+    textAlign: 'center',
+  },
+  content: {
+    padding: 10,
+    backgroundColor: '#ecf0f1',
+    flex: 1,
+  },
+  panel: {
+    marginBottom: 10,
+  },
+  panelText: {
+    paddingBottom: 15,
+  }
 });
+
+// expo init my-app
+// https://reactnative.dev/docs/animated
+// https://reactnative.dev/docs/easing
---
+++ README.md
@@ -0,0 +1,1 @@
+Animated collapsable containers app
